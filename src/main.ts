3237ef6e7e414a5f9a88859f2444f2a032fb90b6
1090504117 <1090504117@qq.com>
2022-02-07 00:44:56 +0800

    repair cors limit

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ async function bootstrap() {
     .setVersion("1.0")
     .build();
   app.setGlobalPrefix("api");
+  app.enableCors({
+    origin: ['http://localhost:4200'],
+  })
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("/", app, document);
 
